a24593688125f9cfa24c86768b692564d0873477
ZubairZubii <zs970120@gmail.com>
2024-08-18 15:19:47 +0500

    part 1 - Login/Signup - Home

--- src/features/product-list/ProductListSlice.js
+++ src/features/product-list/ProductListSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './ProductListAPI';
 
 const initialState = {
   value: 0,
@@ -38,6 +38,5 @@ export const { increment } = counterSlice.actions;
 
 
 export const selectCount = (state) => state.counter.value;
-
 
 export default counterSlice.reducer;
